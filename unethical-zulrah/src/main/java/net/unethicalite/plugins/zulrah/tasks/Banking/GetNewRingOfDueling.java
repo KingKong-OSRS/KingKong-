4da2b51e511032fad16601a4d3b694d538deb40f
KingKong-OSRS <133184076+KingKong-OSRS@users.noreply.github.com>
2023-05-11 14:40:39 +1000

    Update GetNewRingOfDueling.java

--- unethical-zulrah/src/main/java/net/unethicalite/plugins/zulrah/tasks/Banking/GetNewRingOfDueling.java
+++ unethical-zulrah/src/main/java/net/unethicalite/plugins/zulrah/tasks/Banking/GetNewRingOfDueling.java
@@ -1,10 +1,10 @@
+
 package net.unethicalite.plugins.zulrah.tasks.Banking;
 
 import net.runelite.api.Item;
-import net.runelite.api.TileObject;
+import net.runelite.api.NPC;
+import net.runelite.api.coords.WorldPoint;
 import net.unethicalite.api.commons.Time;
-import net.unethicalite.api.entities.NPCs;
-import net.unethicalite.api.entities.TileObjects;
 import net.unethicalite.api.items.Bank;
 import net.unethicalite.api.items.Equipment;
 import net.unethicalite.api.items.Inventory;
@@ -12,55 +12,47 @@ import net.unethicalite.api.plugins.Task;
 import net.unethicalite.plugins.zulrah.data.Constants;
 import net.unethicalite.plugins.zulrah.framework.SessionUpdater;
 
-public class GetNewRingOfDueling extends SessionUpdater implements Task
-{
+public class GetNewRingOfDueling extends SessionUpdater implements Task {
+    private final WorldPoint BANK_CHEST_LOCATION = new WorldPoint(3097, 3495, 0);
+    private final String BANK_CHEST_NAME = "Bank chest";
+
     @Override
-    public boolean validate()
-    {
-        boolean seesBanker = NPCs.getNearest(c -> c.hasAction("Bank")) != null;
+    public boolean validate() {
+        NPC banker = NPCs.getNearest(c -> c.hasAction("Bank"));
+        boolean seesBanker = banker != null && banker.getWorldLocation().distanceTo(getLocalPlayer().getWorldLocation()) < 15;
         boolean noRingOfDueling = !Equipment.contains(c -> c.getName().contains(Constants.RING_OF_DUELING));
 
         return seesBanker && noRingOfDueling;
     }
 
     @Override
-    public int execute()
-    {
+    public int execute() {
         getSession().setCurrentTask("Getting new ring of dueling");
 
-        TileObject bankChest = TileObjects.getNearest(c -> c.hasAction("Use")
-                && c.getName().equalsIgnoreCase("Bank chest"));
+        if (!Bank.isOpen()) {
+            if (!BANK_CHEST_LOCATION.isInScene(getClient())) {
+                getWalking().webWalk(BANK_CHEST_LOCATION);
+                return 1200;
+            }
 
-        if (bankChest == null)
-        {
-            return 1200;
-        }
-
-        if (!Bank.isOpen())
-        {
-            bankChest.interact("Use");
+            Bank.open();
             Time.sleepUntil(() -> Bank.isOpen(), 3600);
         }
-        Time.sleepTicks(1);
-        if (!Inventory.contains(c -> c.getName().startsWith(Constants.RING_OF_DUELING)))
-        {
-            Item ringOfDueling = Bank.getFirst(c -> c.getName().startsWith(Constants.RING_OF_DUELING) && c.getQuantity() > 0);
-            if (ringOfDueling == null)
-            {
 
+        if (!Inventory.contains(c -> c.getName().startsWith(Constants.RING_OF_DUELING))) {
+            Item ringOfDueling = Bank.getFirst(c -> c.getName().startsWith(Constants.RING_OF_DUELING) && c.getQuantity() > 0);
+            if (ringOfDueling == null) {
                 return 600;
             }
 
-            System.out.println(ringOfDueling.getName());
-            Bank.withdraw(ringOfDueling.getName(), 1, Bank.WithdrawMode.ITEM);
-            Time.sleepTicks(1);
-            Bank.close();
+            Bank.withdraw(ringOfDueling.getName(), 1);
         }
 
-        if (!Equipment.contains(c -> c.getName().contains(Constants.RING_OF_DUELING)) && Inventory.contains(c -> c.getName().startsWith(Constants.RING_OF_DUELING)))
-        {
+        if (!Equipment.contains(c -> c.getName().contains(Constants.RING_OF_DUELING)) && Inventory.contains(c -> c.getName().startsWith(Constants.RING_OF_DUELING))) {
             Inventory.getFirst(c -> c.getName().startsWith(Constants.RING_OF_DUELING)).interact("Wear");
         }
+
+        Bank.close();
         return 600;
     }
 }
